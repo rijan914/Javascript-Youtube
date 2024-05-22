9be588a2689a62b26276a102c4512f61cc8e1e17
rijan914 <alpharijan1@gmail.com>
2024-05-22 16:04:50 +0545

    added 01_variables

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 14453
+let accountEmail = "Rijan123@gmail.com"
+var accountPassword ="12355"
+accountCity ="Lanti"
+
+let accountState
+//undefined //
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+// accountId = 3 // not allowed 
+console.log(accountId)
+
+accountEmail ="rijan1@gmail.com"
+accountPassword= "1123231"
+accountCity="salyan"
+
+// console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/* prefer not to write var 
+beacause of issue in block scope and functional scope
+*/ 
