ad3ff88d42d4d5d38f264a7e2cefacb8a10b7f3d
rijan914 <alpharijan1@gmail.com>
2024-05-22 16:36:20 +0545

    added 02_dataTypes

---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 2 + 2)
+// we are using nodejs , not browser
+
+console.log(3+3)
+//code readability should be high
+
+
+let name="rijan"
+let age = 18
+// let isLoggedIn = TRUE 
+
+// number => 2 to the power 53 
+// big int 
+// string => ""
+// boolean => true or false 
+// null => standalone value  eg: let temperature = null
+// undefined => eg: let state; 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof "hitesh")
+
+console.log(typeof undefined)
+// undefined is undefined 
+
+console.log(typeof null)
+//null is object 
