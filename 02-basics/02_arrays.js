7b46b5db51d8e949a9207fb21d776eaae23426a9
rijan914 <alpharijan1@gmail.com>
2024-05-23 13:37:33 +0545

    added 02_arrays

---
+++ 02-basics/02_arrays.js
@@ -0,0 +1,28 @@
+const marvel_heros = ["thor","ironman","spiderman"]
+const dc_heros = ["superman ", "flash ","batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros)
+//array in array 
+
+// const allHeros = marvel_heros.concat(dc_heros) // concatenation
+// console.log(allHeros)
+
+const all_new_heros = [...marvel_heros, ...dc_heros] //spread out values
+console.log(all_new_heros);
+
+const another_array = [1,2,3,[4,5,6],7,[6,7],[4,5]]
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array)
+
+console.log(Array.isArray("hitesh"))
+console.log(Array.from("hitesh")) //converts into array 
+console.log(Array.from({name:"rijan"}))  //intersting 
+
+let score1= 100 
+let score2 = 200
+let score3 = 300 
+
+console.log(Array.of(score1,score2,score3))
+
