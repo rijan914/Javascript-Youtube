7f8189e1ed12d93a7dca8566561bc19d451936c3
rijan914 <alpharijan1@gmail.com>
2024-06-03 14:04:21 +0545

    added function

---
+++ 03-basics/01_functions.js
@@ -0,0 +1,44 @@
+
+
+// function sayMyName(){
+//     console.log("r");
+//     console.log("i");
+//     console.log("v");
+//     console.log("n");
+//     console.log("a");
+//     console.log("j");
+// }
+
+// sayMyName() //reference // execution
+
+// // function addTwoNumbers(number1 , number2)//parameters 
+// // {
+// //     console.log(number1 + number2);
+
+// // }
+// function addTwoNumbers(number1 , number2)//parameters 
+// {
+// //    let result = number1+number2
+// //    return result
+// //     console.log("rijan") //unreacheable code
+//     return number1+number2
+
+// }
+
+// const result = addTwoNumbers(3,2) //arguments 
+// // console.log("result",result)
+// // addTwoNumbers("3","2")
+// // addTwoNumbers("3",null)
+
+
+function loginUserMessage(username="sam")
+{//username === undefined
+    if(!username)
+    {
+        console.log("please enter a username");
+        return 
+    }
+    return`${username} just logged in`  //using string interpolation
+}
+console.log(loginUserMessage("Rijan"))
+// console.log(loginUserMessage())
